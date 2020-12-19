9c641f2ea0d4d45c3a5b6e9f0f7470f3727b85e2
Robin van Leeuwen <robin@mintlab.nl>
2020-12-19 11:54:06 +0100

    initial commit

---
+++ migrations/2020-12-19-104246_runcount/up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE runcount (
+    id SERIAL NOT NULL PRIMARY KEY,
+    run INTEGER,
+    count INTEGER
+);
---
+++ src/schema.rs
@@ -0,0 +1,7 @@
+table! {
+    runcount (id) {
+        id -> Int4,
+        run -> Nullable<Int4>,
+        count -> Nullable<Int4>,
+    }
+}
